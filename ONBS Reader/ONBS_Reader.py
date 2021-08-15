b40869da901ca916b57beb5a5b2f6f42291590ec
Adelemphii <demilitioowner@gmail.com>
2021-08-15 09:30:55 -0500

    it'll work w/ the whole file AS LONG as there aren't multiple notes per layer and ofc if the pause between notes isn't longer than 60 ticks

--- ONBS Reader/ONBS_Reader.py
+++ ONBS Reader/ONBS_Reader.py
@@ -3,28 +3,85 @@ import pynbs
 
 demo_song = pynbs.read('demo_song.nbs')
 
-list = []
+note_list = []
+chars = ['[', ']']
 
 previous_tick = 0
 
 for tick, chord in demo_song:
 	if tick == 0:
-		list.append([note.key for note in chord])
+		note_list.append([note.key for note in chord])
 
 	elif tick - previous_tick >= 20:
-		list.append(25)
-		list.append([note.key for note in chord])
+		if tick - previous_tick >= 40:
+			note_list.append(25)
+			note_list.append(25)
+			if tick-previous_tick >= 60:
+				note_list.append(25)
+				note_list.append(25)
+				note_list.append(25)
+		note_list.append(25)
+		note_list.append([note.key for note in chord])
 		previous_tick = tick
 
 	else:
-		list.append([note.key for note in chord])
+		note_list.append([note.key for note in chord])
 		previous_tick = tick
 	print(tick, [note.key for note in chord])
 
 print()
-print('All Notes:', list)
+print('All Notes:', note_list)
 
 print()
-print()
-print(demo_song.layers[0])
-print(demo_song.instruments[0])
+
+def define_numbers(argument):
+	return {
+		25: 25,
+        33: 0,
+        34: 1,
+        35: 2,
+        36: 3,
+        37: 4,
+        38: 5,
+        39: 6,
+        40: 7,
+        41: 8,
+        42: 9,
+        43: 10,
+        44: 11,
+        45: 12,
+        46: 13,
+        47: 14,
+        48: 15,
+        49: 16,
+        50: 17,
+        51: 18,
+        52: 19,
+        53: 20,
+        54: 21,
+        55: 22,
+        56: 23,
+        57: 24
+	}[argument]
+
+def flatten(xs):
+    result = []
+    if isinstance(xs, (list, tuple)):
+        for x in xs:
+            result.extend(flatten(x))
+    else:
+        result.append(xs)
+    return result
+note_list = flatten(note_list)
+formatted_note_list = []
+# 33 is lowest - 0
+# 57 is highest - 24
+for note_num in note_list:
+    formatted_note_list.append(define_numbers(note_num))
+    print(define_numbers(note_num))
+    print(note_num)
+
+
+with open('file.txt', 'w') as f:
+	for item in formatted_note_list:
+		f.write("%s," % item)
